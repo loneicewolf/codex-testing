2235a146397ae50869e782b9e6b75669b01fd902
0AA01A0F1 <68499986+loneicewolf@users.noreply.github.com>
2025-06-04 09:11:20 -0400

    a
    delete to follow rules even if this isnt breaking anythinga

--- sslkeylog/windows/set_sslkeylog.ps1
+++
@@ -1,8 +0,0 @@
-# PowerShell script to set SSLKEYLOGFILE for the current session
-# Usage: .\set_sslkeylog.ps1 [log_file]
-
-param(
-    [string]$LogFile = "$env:USERPROFILE\sslkeylog.log"
-)
-$env:SSLKEYLOGFILE = $LogFile
-Write-Output "SSLKEYLOGFILE set to $env:SSLKEYLOGFILE"
